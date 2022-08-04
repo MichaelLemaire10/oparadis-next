6099b864de5117ff64ad16b58233ce9f05be79b7
Michael Lemaire10 <leahcim60@outlook.fr>
2022-08-04 17:23:36 +0200

    feature: implementation redux/query in progress

---
+++ src/services/animal.js
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// Define a service using a base URL and expected endpoints
+export const animalApi = createApi({
+  reducerPath: 'animalApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+  endpoints: (builder) => ({
+    getAnimal: builder.query({
+      query: () => `animal`,
+    }),
+  }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { getAnimal } = animalApi
---
+++ src/services/plant.js
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// Define a service using a base URL and expected endpoints
+export const plantApi = createApi({
+  reducerPath: 'plantApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+  endpoints: (builder) => ({
+    getPlant: builder.query({
+      query: () => `plant`,
+    }),
+  }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { getPlant } = plantApi
