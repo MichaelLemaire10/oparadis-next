9870791c8b3ba30e1cf18a15842fcc035fa068fe
Michael Lemaire10 <leahcim60@outlook.fr>
2022-09-14 16:59:55 +0200

    features: setting up e2e test with cypress

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: 'a932qx',
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    supportFile: false,
+    specPattern: 'cypress/*.cy.{js,jsx}',
+  }
+})
---
+++ cypress/spec.cy.js
@@ -0,0 +1,5 @@
+describe('index.spec.cy.js', () => {
+    it('should visit', () =>{
+        cy.visit('/')
+    })
+})
